44d2f99eb154cf411fc17a6a3d0606cef904aeb3
vaishnavipagire <158028893+vaishnavipagire@users.noreply.github.com>
2026-04-10 18:47:55 +0530

    updated files

--- src/screens/AddUserScreen.jsx
+++ src/screens/AddUserScreen.jsx
@@ -1,160 +1,217 @@
-import { useState , useRef } from 'react';
-import { View, Text, StyleSheet,TouchableOpacity ,TextInput,Image} from 'react-native';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
-const AddUserScreen = ({navigation}) => {
-  
-   const [text, setText ] = useState();
-   const[selectedItem,setSelectedItem] = useState('admin');
-  //  const pickerRef = useRef();
-   const route = useRoute();
-   console.log(route.params);
-   
-   return (
+const AddUserScreen = () => {
+  const navigation = useNavigation();
+  const [selectedItem, setSelectedItem] = useState();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('');
+  const [phone, setPhone] = useState('');
+  const [status, setStatus] = useState('');
+
+  console.log(name, email);
+
+  const postAPIData = async()=>{
+  const url = 'https://69c275e57518bf8facbe6b7a.mockapi.io/users/userList';
+  let result = await fetch(url,{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify(
+      {
+        name,
+        email,
+        role,
+        phone,
+        status,
+      }
+    )
+  });
+   result = await result.json();
+   console.log(result);
+     }
+     useEffect(() => {
+     postAPIData();
+       }, []);
+
+  return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity onPress={()=>navigation.navigate('UserListScreen')}>
+        <TouchableOpacity
+          onPress={() => {
+            postAPIData();
+            navigation.navigate('UserListScreen');
+          }}
+        >
           <Text style={styles.text}>Cancel</Text>
         </TouchableOpacity>
-        <Text style={styles.text1}>Add/EditUser</Text>
-        <TouchableOpacity>
-          <Text style={styles.text2}>save</Text>
+        <Text style={styles.text1}>Add/Edit User</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('UserListScreen')}>
+          <Text style={styles.text2}>Save</Text>
         </TouchableOpacity>
-         </View>
-    
-     <View>
-       <TouchableOpacity>
-          <View style={styles.photo}>
-           <Image source={require('../assets/Imageicon.jpg')} 
-           style={styles.imageicon}/>
-           <View styles={styles.photo1}>
-             <Text style={{color:'blue'}}>Edit photo</Text>
-           </View>
-          {/* <Text style={{color:'blue'}}>Edit photo</Text> */}
-         </View>
-         </TouchableOpacity>
-    </View>
-      
-      <View style={styles.container1}>
-      <Text style={{fontSize:18}}>Full Name</Text>
-      <TextInput style={styles.textinput} 
-       placeholder="Enter your name" 
-      />
-      
-      <Text style={{fontSize:18}}>Email</Text>
-      <TextInput style={styles.textinput} 
-      placeholder="Enter your email" 
-       />
-
-      <View style={styles.container2}>
-       <Text style={{fontSize:19}}>Role</Text>
-      <View style={{left:150}}>
-      <View style={styles.roleDropdownList}>
-      <Picker 
-      // ref={pickerRef}
-      selectedValue={selectedItem}
-      onValueChange={(itemValue)=> setSelectedItem(itemValue)}>
-        <Picker.Item label='Admin' value='Admin'/>
-         <Picker.Item label='Manager' value='Manager'/>
-         <Picker.Item label='Administrator' value='Administrator'/>
-      </Picker>
-      </View>
-      </View>
-       </View>
-
-     <View>
-       <View style={styles.container3}>
-      <Text style={{fontSize:18,}}>Phone Number</Text>
-      <View style={{position:'absolute',left:369}}>
-      <TextInput style={styles.textinput2}
-      placeholder='+9124567899'/>
-      </View>
       </View>
 
-      <View style={styles.container4}>
-     <Text style={{fontSize:18}}>Status</Text>
-       <View style={{position:'absolute',left:226}}>
-      <TextInput style={styles.textinput3}/>
-      <View style={styles.statusDropdownList}>
-      <Picker 
-      // ref={pickerRef}
-      selectedValue={selectedItem}
-      onValueChange={(itemValue)=> setSelectedItem(itemValue)}>
-        <Picker.Item label='Active' value='Active'/>
-         <Picker.Item label='Inactive' value='Inactive'/>
-         <Picker.Item label='All' value='All'/>
-      </Picker>
+      <View style={styles.hzline}>
       </View>
-       </View>
-      </View>
-     </View>
-    </View>
-   
-  <TouchableOpacity style={styles.deletebutton}>
-        <Text style={{color:'white'}}>Delete User</Text>
+
+      <View>
+        <View>
+          <Image
+            source={require('../assets/Imageicon.jpg')}
+            style={styles.imageicon}/>
+        </View>
+
+        <TouchableOpacity style={styles.edit}>
+          <Text style={{ color: 'blue', left: 15, top: 5 }}>Edit photo</Text>
         </TouchableOpacity>
+      </View>
+
+       <View style={styles.hzline1}>
+      </View>
+      <View style={{ top:36}}>
+        <Text style={{ fontSize: 17, top: 10 }}>Full Name</Text>
+        <TextInput
+          style={styles.textinput}
+          placeholder="Enter your name"
+          value={name}
+          onChangeText={text => setName(text)}/>
+         
+        <Text style={{ fontSize: 17, top: 15}}>Email</Text>
+        <TextInput
+          style={styles.textinput1}
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={text => setEmail(text)}/>
+
+       <View style={styles.hzline3}>
+      </View>
+        <View style={styles.rolecontainer}>
+          <Text style={{ fontSize: 19}}>Role</Text>
+          <View style={{ left: 166 }}>
+            <View style={styles.roleDropdownList}>
+              <Picker
+                selectedValue={selectedItem}
+                onValueChange={itemValue => setSelectedItem(itemValue)} >
+                <Picker.Item label="Admin" value="Admin" />
+                <Picker.Item label="Manager" value="Manager" />
+                <Picker.Item label="Administrator" value="Administrator" />
+              </Picker>
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <View style={styles.phonecontainer}>
+            <Text style={{ fontSize: 18 }}>Phone Number</Text>
+            <View style={{ position: 'absolute', left: 380 }}>
+              <TextInput
+                style={styles.phoneinput}
+                placeholder="+9124567899"
+                onChangeText={value => setPhone(value)}
+                value={phone} />
+            </View>
+          </View>
+
+       <View style={styles.statuscontainer}>
+            <Text style={{ fontSize: 18 }}>Status</Text>
+            <View style={{ position: 'absolute', left: 240 }}>
+              <TextInput
+                style={styles.statustextinput3}
+                value={role}
+                onChangeText={text => setStatus(text)}
+              />
+              <View style={styles.statusinput}>
+                <Picker
+                  selectedValue={selectedItem}
+                  onValueChange={itemValue => setSelectedItem(itemValue)} >
+                  <Picker.Item label="Active" value="Active" />
+                  <Picker.Item label="Inactive" value="Inactive" />
+                  <Picker.Item label="All" value="All" />
+                </Picker>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.hzline6}>
+      </View>
+      <TouchableOpacity style={styles.deletebutton}>
+        <Text style={{ color: 'white' }}>Delete User</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-        //  flex:1,
-        padding:20,
+    //  flex:1,
+    padding: 20,
   },
-  header:{
+  header: {
     justifyContent: 'space-evenly',
     alignItems: 'center',
     flexDirection: 'row',
-    paddingTop:0,
+    paddingTop: 10,
   },
   text: {
-    paddingLeft:0,
-    paddingRight:100,
+    paddingLeft: 0,
+    paddingRight: 100,
     fontSize: 20,
   },
   text1: {
-     paddingRight:40,
+    paddingRight: 30,
     fontWeight: 'bold',
-    fontSize: 20,
+    fontSize:20,
+    right: 30,
   },
-  text2:{
-      fontSize:20,
-      backgroundColor:'blue',
-      color:'white',
-       paddingHorizontal:10,
-      borderRadius:4,
+  text2: {
+    fontSize:20,
+    backgroundColor:'#2b50c0',
+    color: 'white',
+    paddingHorizontal: 10,
+    borderRadius:6,
   },
-  // container1:{
-  //   fontSize:20,
-  //   paddingVertical:10,
-  //  },
-  photo:{
-       borderWidth: 1,
-       borderColor: 'black',
-       borderRadius: 4,
-       padding:5,
-      marginLeft:130,
-      marginRight:160,
-      marginTop:10,
-      justifyContent:'center',
-       
- },
-    imageicon:{
-       width: 100,
-       height: 100, 
-       borderRadius:4
-    },
-    photo1:{
-       borderWidth: 1,
-       borderColor: 'black',
-       borderRadius: 4,
-       padding:5,
-      marginLeft:130,
-      marginRight:160,
-      marginTop:10,
-      justifyContent:'center',
-    },
+  photo: {
+    height: 200,
+    width: 200,
+    left: 150,
+    top: 20,
+  },
+  imageicon: {
+    width: 130,
+    height: 130,
+    borderRadius: 100,
+    left: 120,
+    top: 17,
+  },
+  edit: {
+    borderWidth: 0.5,
+    height: 25,
+    width: 100,
+    left: 138,
+    borderColor: 'grey',
+    borderRadius: 5,
+    top: 23,
+    paddingBottom:8,
+  },
+  hzline1:{
+    height:1,
+    width:500,
+    right:20,
+    backgroundColor:'grey',
+    top:35,
+  },
   button: {
     alignItems: 'center',
     backgroundColor: 'red',
@@ -164,95 +221,124 @@ const styles = StyleSheet.create({
     paddingHorizontal: 130,
     paddingVertical: 6,
   },
-  container1:{
-    paddingVertical:1,
-    //  margin:0,
-   },
-  textinput:{
-    fontSize:13,
+  textinput: {
     borderWidth: 1,
-    borderColor:'black',
+    top: 15,
+    marginBottom: 10,
     borderRadius: 4,
-    padding:6,
-    height:32,
-    width:360, 
-    flexDirection:'row',
-  },
-  container2:{
-      //width:200,
-      flexWrap:"wrap",
-      flexDirection:"row",
-      marginLeft:10,
-      marginTop:10
-   },
-  roleDropdownList:{
-     borderWidth:1,
-     width:161,
-     height:39 ,
-     marginBottom:4,
-     borderRadius:4,
-     padding:2,
-  },
- container3:{
-      width:200,
-      flexWrap:"wrap",
-      flexDirection:"row",
-      margin:0,
-      padding:5,
-      },
- textinput2:{
-    position:'absolute',
-    right:2,
-    borderWidth: 1,
-    borderColor:'black',
-    borderRadius:4,
-    margin:8,
-    padding:6,
-    height: 30,
-    width:190,
-    flexDirection:"row",
-},
-  container4:{
-      width:200,
-      flexWrap:"wrap",
-      flexDirection:"row",
-      padding:15,
-  },
-textinput3:{
-  position:'absolute',
-    borderWidth: 1,
-    borderColor:'black',
-    borderRadius:4,
+    borderColor:'grey',
     padding:10,
-    margin:13,
-    height:30,
-    width:120,
-    paddingVertical:4,
-   flexWrap:"wrap",
-   flexDirection:"row",
- },
- statusDropdownList:{
-     position:'absolute',
-     height:110,
-     width:140,
-     margin:2,
-     padding:1,
-    paddingHorizontal:8,
- },
- container5:{
-      flex:1,
- },
-deletebutton:{
-         position:'absolute',
-         backgroundColor:'red',
-         padding:10,
-         right:40,
-         borderRadius:4,
-         fontSize:20,
-         paddingVertical:6,
-        paddingHorizontal:130,
-        marginTop:850,
-        Color:'white',
- },
- });
+  },
+  textinput1:{
+    borderWidth: 1,
+    top: 20,
+    marginBottom: 10,
+    borderRadius: 4,
+    borderColor:'grey',
+    padding:11,
+  },
+  hzline3:{
+     height:1,
+    width:500,
+    right:20,
+    backgroundColor:'grey',
+    top:20,
+  },
+  rolecontainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    marginTop: 30,
+    padding:2,
+  },
+  roleDropdownList: {
+    borderWidth: 1,
+    borderColor:'grey',
+    width: 165,
+    height: 39,
+    marginBottom: 1,
+    borderRadius: 4,
+    marginTop: 3,
+    paddingHorizontal: 1,
+  },
+  phonecontainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    marginTop: 6,
+    padding: 1,
+    paddingVertical: 10,
+  },
+  phoneinput: {
+    position: 'absolute',
+    right: 2,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    margin: 8,
+    padding: 10,
+    height: 39,
+    width: 165,
+    flexDirection: 'row',
+  },
+  statuscontainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    marginTop: 4,
+    padding: 5,
+    paddingVertical: 20,
+  },
+  statustextinput3: {
+    position: 'absolute',
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    margin: 13,
+    height: 39,
+    width: 116,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  statusinput: {
+    position: 'absolute',
+    height: 110,
+    width: 140,
+    margin: 10,
+    paddingHorizontal: 8,
+  },
+  hzline6:{
+    height:1,
+    width:500,
+    right:20,
+    backgroundColor:'grey',
+    top:30,
+  },
+  container5: {
+    flex: 1,
+  },
+  hzline6:{
+    height:1,
+    width:500,
+    right:20,
+    backgroundColor:'grey',
+    top:31,
+  },
+  deletebutton: {
+    position: 'absolute',
+    backgroundColor: '#e32f2fdd',
+    padding: 10,
+    right: 40,
+    borderRadius:6,
+    fontSize: 20,
+    paddingVertical: 6,
+    paddingHorizontal: 130,
+    marginTop: 850,
+    Color: 'white',
+  },
+  hzline:{
+    height:1,
+    width:500,
+    right:20,
+    backgroundColor:'grey',
+    top:6,
+  }
+});
 export default AddUserScreen;
--- src/screens/UserListScreen.jsx
+++ src/screens/UserListScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React,{ useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -6,41 +6,58 @@ import {
   StyleSheet,
   TextInput,
   Image,
-  TouchableOpacity,
-} from 'react-native';
+  TouchableOpacity,ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/Feather';
 import Squarecircle from 'react-native-vector-icons/dist/Feather';
 import Searchicon from 'react-native-vector-icons/dist/EvilIcons';
 import { useNavigation } from '@react-navigation/native';
 
-const UserListScreen = () => {
-  const Navigation = useNavigation();
+  const UserListScreen = () => {
+  const navigation = useNavigation();
 
   const [data, setData] = useState();
   const [filteredData, setFilteredData] = useState([]);
   const [search, setSearch] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  const getAPIData = async () => {
-    const url = 'https://69c275e57518bf8facbe6b7a.mockapi.io/users/user';
-    let result = await fetch(url);
-    result = await result.json();
-    setData(result);
-    setFilteredData(result);
+   const getAPIData = async () => {
+    try{
+       const url = 'https://69c275e57518bf8facbe6b7a.mockapi.io/users/userList';
+      let result = await fetch(url);
+      result = await result.json();
+      setData(result);
+     setFilteredData(result);
+    }
+    catch (error) {
+      console.error("Fetch error:", error);
+    }finally {
+      setLoading(false);
+      setRefreshing(false);
+    }
   };
   useEffect(() => {
     getAPIData();
   }, []);
 
+   const onRefresh = () => {
+    setRefreshing(true);
+    getAPIData(); 
+  };
+
   const handleSearch = text => {
     setSearch(text);
-
+    if (!text) {
+      setFilteredData(data);
+      return;
+    }
     const filtered = data.filter(item =>
-      item.name.toLowerCase().includes(text.toLowerCase()),
+      item.name.toLowerCase().includes(text.toLowerCase())
     );
     setFilteredData(filtered);
   };
 
-  const renderUserItem = ({ item }) => (
+    const renderItem = ({ item }) => (
     <View style={styles.container1}>
       <Image source={{ uri: item.avatar }} style={styles.image} />
       <View style={styles.Container3}>
@@ -48,41 +65,45 @@ const UserListScreen = () => {
         <Text style={{ fontSize: 16 }}>{item.email}</Text>
         <Text style={{ fontSize: 16 }}>{item.phone}</Text>
       </View>
-
-      <TouchableOpacity
+   {/* <TouchableOpacity
         style={styles.deletebutton}
-        onPress={() => Navigation.navigate('AddUserScreen')}>
+        onPress={() => navigation.navigate('AddUserScreen',{item})}>
         <Text style={{ color: 'white' }}>Delete</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.updatebutton}
-       onPress={() => Navigation.navigate('AddUserScreen',{item:item})}>
+
+      <TouchableOpacity
+        style={styles.updatebutton}
+        onPress={() => navigation.navigate('AddUserScreen', { item: item })}
+      >
         <Text style={{ color: 'white' }}>Update</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
+  if (loading) {
+    return <ActivityIndicator size="large" style={{ flex: 1 }} />;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.text}>All Users</Text>
-      <Icon name="plus" size={20} style={styles.icon} />
+      <Icon name="plus" size={17} style={styles.icon}
+      onPress={() => navigation.navigate('AddUserScreen')}
+      />
+  
       <Searchicon name="search" size={30} style={styles.searchicon} />
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Search users"
-        value={search}
-        onChangeText={handleSearch}
+      <TextInput style={styles.searchBar} placeholder="Search users"
+        value={search} onChangeText={handleSearch}
       />
-      <Squarecircle
-        name="plus-circle"
-        size={40}
-        style={styles.circleicon}
-        onPress={() => Navigation.navigate('AddUserScreen')}
-      />
-      <FlatList
+      <Squarecircle name="plus" size={45} style={styles.circleicon}
+        onPress={() => navigation.navigate('AddUserScreen')}/>
+        {/* <Text>{'>'} </Text> */}
+       <FlatList
         data={filteredData}
-        keyExtractor={item => item.id}
-        renderItem={renderUserItem}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
       />
-    </View>
+      </View>
   );
 };
 export default UserListScreen;
@@ -92,15 +113,16 @@ const styles = StyleSheet.create({
     flex: 1,
     border: 1,
     padding: 3,
+    top:15,
   },
   text: {
     margintop: 10,
     fontSize: 21,
-    top: 10,
-    left: 20,
+    top: 12,
+    left: 10,
     fontWeight: 'bold',
   },
-  searchBar: {
+    searchBar: {
     paddingHorizontal: 40,
     paddingVertical: 8,
     borderRadius: 6,
@@ -115,10 +137,18 @@ const styles = StyleSheet.create({
   icon: {
     position: 'absolute',
     top: 21,
-    left: 380,
-    right: 0,
-    bottom: 0,
-  },
+    height:22,
+    width:22,
+    left: 370,
+    color:'grey',
+    bottom:870,
+    borderWidth:2,
+    borderRadius:1,
+    borderColor:'grey',
+ },
+ container1:{
+   backgroundColor:'#DCDCDC',
+ },
   searchicon: {
     position: 'absolute',
     margin: 56,
@@ -126,12 +156,15 @@ const styles = StyleSheet.create({
     left: 6,
   },
   circleicon: {
-    position: 'absolute',
-    fontSize: 40,
-    right: 0,
-    bottom: 0,
-    zIndex: 1,
-    // top:900,
+  color:'white',
+    backgroundColor:'blue',
+    top: 720,
+    height:50,
+    width:50,
+    left: 343,
+    borderWidth:1,
+    borderRadius:30,
+    borderColor:'grey',
   },
   item: {
     fontSize: 18,
@@ -140,36 +173,35 @@ const styles = StyleSheet.create({
   container1: {
     flex: 1,
     padding: 15,
-    // paddingLeft:170,
     fontWeight: 'bold',
   },
   Container3: {
     position: 'absolute',
     marginLeft: 100,
-    padding: 6,
+    padding:6,
   },
-    updatebutton: {
-    position: 'absolute',
-    backgroundColor: 'red',
-    borderRadius: 4,
-    paddingVertical: 3,
-    height: 25,
-    width: 65,
-    right: 0,
-    padding: 10,
-    top: 20,
-  },
-  deletebutton: {
-    position: 'absolute',
-    backgroundColor: 'red',
-    borderRadius: 4,
-    paddingVertical: 3,
-    height: 25,
-    width: 65,
-    right: 0,
-    padding: 10,
-    bottom: 10,
-  },
+  // updatebutton: {
+  //   position: 'absolute',
+  //   backgroundColor: 'red',
+  //   borderRadius: 4,
+  //   paddingVertical: 3,
+  //   height: 25,
+  //   width: 65,
+  //   right: 0,
+  //   padding: 10,
+  //   top: 20,
+  // },
+  // deletebutton: {
+  //   position: 'absolute',
+  //   backgroundColor: 'red',
+  //   borderRadius: 4,
+  //   paddingVertical: 3,
+  //   height: 25,
+  //   width: 65,
+  //   right: 0,
+  //   padding: 10,
+  //   bottom: 10,
+  // },
   image: {
     height: 60,
     width: 60,
