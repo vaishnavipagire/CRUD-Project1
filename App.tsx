9897c9aede7557abf8ae0557637f41a9b967c328
vaishnavipagire <158028893+vaishnavipagire@users.noreply.github.com>
2026-05-06 15:43:34 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import UserListScreen from './src/screens/UserListScreen'
-import AddUserScreen from './src/screens/AddUserScreen'
+import AppNavigator from './src/navigation/AppNavigator';
 
-const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-       <Stack.Navigator>
-         <Stack.Screen name="UserListScreen" component={UserListScreen} 
-         options={{
-          headerShown:false,
-         headerTitleAlign:'center'
-        }}/> 
-        <Stack.Screen name="AddUserScreen" component={AddUserScreen}
-       options={{
-        headerShown:false,
-         headerTitleAlign:'center'
-        }}  
-        />
-       </Stack.Navigator>
-     </NavigationContainer>
+   <AppNavigator/>
    );
 };
 export default App;
